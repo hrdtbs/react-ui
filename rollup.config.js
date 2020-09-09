f77a690664f1506908b46f473277ef3e3ddafc38
hrdtbs <harada.tsubasa.0422@gmail.com>
2020-09-09 17:50:10 +0900

    chore: add linters

--- rollup.config.js
+++ rollup.config.js
@@ -3,33 +3,35 @@ import pkg from "./package.json"
 import resolve from "@rollup/plugin-node-resolve"
 import typescript from "rollup-plugin-typescript2"
 
+/** @type {import('rollup').RollupOptions} */
 const options = {
     input: "src/index.tsx",
     external: Object.keys(pkg.peerDependencies || {}),
     plugins: [
         typescript({
             typescript: require("typescript"),
-            tsconfig: "./tsconfig.json"
+            tsconfig: "./tsconfig.build.json",
         }),
         resolve(),
         babel({
             exclude: [/\/core-js\//, "node_modules/**"],
             presets: [
                 "@babel/preset-react",
+                "@babel/plugin-transform-react-constant-elements",
                 [
                     "@babel/preset-env",
                     {
                         useBuiltIns: "usage",
-                        corejs: 3
-                    }
-                ]
-            ]
-        })
+                        corejs: 3,
+                    },
+                ],
+            ],
+        }),
     ],
     output: [
         { file: `${pkg.main}`, format: "cjs" },
-        { file: `${pkg.module}`, format: "es" }
-    ]
+        { file: `${pkg.module}`, format: "es" },
+    ],
 }
 
 export default [options]
