448e033f4fad99f3884b36b121d0997d4c04a6ed
hrdtbs <harada.tsubasa.0422@gmail.com>
2020-09-26 11:46:29 +0900

    chore(stories): change directory

--- stories/Button.stories.tsx
+++ stories/Button.stories.tsx
@@ -2,7 +2,7 @@ import React from "react"
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
 import { Meta, Story } from "@storybook/react/types-6-0"
 
-import { Button, ButtonProps } from "./Button"
+import { Button, ButtonProps } from "../src/Button"
 
 export default {
     title: "Example/Button",
